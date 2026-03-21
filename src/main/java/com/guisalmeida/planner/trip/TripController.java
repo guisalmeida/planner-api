51f8bb2f35aaae5d90fd76365ecdc8e61c07d108
guisalmeida <guisalmeida.dev@gmail.com>
2026-03-21 22:36:24 +0000

    feat(trip): finish trip endpoints

--- src/main/java/com/guisalmeida/planner/trip/TripController.java
+++ src/main/java/com/guisalmeida/planner/trip/TripController.java
@@ -3,10 +3,12 @@ package com.guisalmeida.planner.trip;
 import com.guisalmeida.planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/trips")
@@ -19,13 +21,52 @@ public class TripController {
     private TripRepository tripRepository;
 
     @PostMapping
-    public ResponseEntity<String> createTrip(@RequestBody TripRequestPayload payload) {
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload) {
         Trip newTrip = new Trip(payload);
 
         this.tripRepository.save(newTrip);
-
         this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
-        return ResponseEntity.ok("Success");
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{tripId}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID tripId) {
+        Optional<Trip> trip = this.tripRepository.findById(tripId);
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{tripId}")
+    public ResponseEntity<Trip> updateTripDetails(@PathVariable UUID tripId, @RequestBody TripRequestPayload payload) {
+        Optional<Trip> trip = this.tripRepository.findById(tripId);
+
+        if(trip.isPresent()) {
+            Trip updatedTrip = trip.get();
+
+            updatedTrip.setDestination(payload.destination());
+            updatedTrip.setStartsAt(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            updatedTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+
+            this.tripRepository.save(updatedTrip);
+            this.participantService.triggerConfirmationEmailToParticipant(tripId);
+
+            return ResponseEntity.ok(updatedTrip);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{tripId}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable UUID tripId) {
+        Optional<Trip> trip = this.tripRepository.findById(tripId);
+
+        if(trip.isPresent()) {
+            Trip updatedTrip = trip.get();
+
+            updatedTrip.setConfirmed(true);
+
+            this.tripRepository.save(updatedTrip);
+            return ResponseEntity.ok(updatedTrip);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
---
+++ src/main/java/com/guisalmeida/planner/trip/TripCreateResponse.java
@@ -0,0 +1,6 @@
+package com.guisalmeida.planner.trip;
+
+import java.util.UUID;
+
+public record TripCreateResponse(UUID tripId) {
+}
