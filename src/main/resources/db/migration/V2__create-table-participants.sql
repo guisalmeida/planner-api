93c5f4a71b46c92ca25e330d6aabb76ac2b8a65e
guisalmeida <guisalmeida.dev@gmail.com>
2026-03-29 23:11:59 +0100

    chore(actitvities): create table

--- src/main/resources/db/migration/V2__create-table-participants.sql
+++ src/main/resources/db/migration/V2__create-table-participants.sql
@@ -1,8 +1,9 @@
-CREATE TABLE participants (
+CREATE TABLE participants
+(
     id           UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
     name         VARCHAR(255) NOT NULL,
     email        VARCHAR(255) NOT NULL,
     is_confirmed BOOLEAN      NOT NULL,
-    trip_Id UUID,
-    FOREIGN KEY (trip_Id) REFERENCES trips(id)
+    trip_Id      UUID,
+    FOREIGN KEY (trip_Id) REFERENCES trips (id) ON DELETE CASCADE
 );
